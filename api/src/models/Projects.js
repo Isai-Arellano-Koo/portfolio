207fd80c11d624e35e2ace9cea4a09438bb0e075
Isai <isakoospike@gmail.com>
2023-11-03 11:07:09 -0500

    cambio en url base de datos

---
+++ api/src/models/Projects.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+    sequelize.define('project', {
+        id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
+    }, {timestamps: false})
+}
