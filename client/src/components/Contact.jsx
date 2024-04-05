0b57653acd3bf5c630b53d234e0c3f7b233751e3
Isai-Arellano-Koo <isakoospike@gmail.com>
2024-04-05 11:30:29 -0500

    s

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { FaGithub, FaLinkedin, FaEnvelope  } from "react-icons/fa";
-import gifJson from '../../Comp 1.json'
-import Bodymovin from 'bodymovin';
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import Lottie from 'react-lottie';
+import gifJson from '../../Comp 1.json';
 
 const Contact = () => {
-  const [t, i18n] = useTranslation("global")
-
+  const [t, i18n] = useTranslation("global");
 
   return (
     <div className='text-white pt-40 flex flex-col ml-10 lg:ml-48 mr-10 max-w-6xl mb-20'>
@@ -16,26 +15,25 @@ const Contact = () => {
       <h3 className="text-3xl lg:text-6xl text-color-links font-viet text-center">{t("contact.text")}</h3>
       {/* Utiliza el componente Lottie para renderizar la animación */}
       {gifJson && (
-        <Bodymovin animationData={gifJson} loop={true} />
+        <Lottie animationData={gifJson} loop={true} />
       )}
       <div className="flex gap-5 justify-center mt-5">
-      <div className="my-4">
-        <a target="_blank" className="text-8xl hover:text-green" href="https://github.com/Isai-Arellano-Koo">
-          <FaGithub /> 
-        </a>
+        <div className="my-4">
+          <a target="_blank" className="text-8xl hover:text-green" href="https://github.com/Isai-Arellano-Koo">
+            <FaGithub />
+          </a>
+        </div>
+        <div className="my-4">
+          <a target="_blank" className="text-8xl hover:text-green" href="https://www.linkedin.com/in/isai-arellano/">
+            <FaLinkedin />
+          </a>
+        </div>
+        <div className="my-4">
+          <a target="_blank" className="text-8xl hover:text-green" href="mailto:arko.isai19@gmail.com">
+            <FaEnvelope />
+          </a>
+        </div>
       </div>
-      <div className="my-4">
-        <a target="_blank" className="text-8xl hover:text-green" href="https://www.linkedin.com/in/isai-arellano/">
-          <FaLinkedin />
-        </a>
-      </div>
-      <div className="my-4">
-        <a target="_blank" className="text-8xl hover:text-green" href="mailto:arko.isai19@gmail.com">
-          <FaEnvelope />
-        </a>
-      </div>
-      </div>
-      
     </div>
   );
 };
